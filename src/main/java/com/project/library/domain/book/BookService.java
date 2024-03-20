b07bc0905f5aaae4ccb497db72d4b7ecb1636c77
songseongbin <ssbin0916@naver.com>
2024-03-20 09:02:39 +0900

    도메인 패키지로 수정

--- src/main/java/com/project/library/domain/book/BookService.java
+++ src/main/java/com/project/library/domain/book/BookService.java
@@ -17,5 +17,28 @@ public class BookService {
         return bookMapper.findAll();
     }
 
-    
+    @Transactional(readOnly = true)
+    public Book findById(Long id) throws Exception {
+        return bookMapper.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public Book findByTitle(String title) throws Exception {
+        return bookMapper.findByTitle(title);
+    }
+
+    @Transactional
+    public void save(Book book) throws Exception {
+        bookMapper.insert(book);
+    }
+
+    @Transactional
+    public void update(Book book) throws Exception {
+        bookMapper.update(book);
+    }
+
+    @Transactional
+    public void delete(Long id) throws Exception {
+        bookMapper.delete(id);
+    }
 }
