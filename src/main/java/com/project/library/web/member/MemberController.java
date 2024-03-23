edb484743193159d6c4f62ac87e06004697b441c
songseongbin <ssbin0916@naver.com>
2024-03-23 17:07:07 +0900

    refactor: MemberController 수정

--- src/main/java/com/project/library/web/member/MemberController.java
+++ src/main/java/com/project/library/web/member/MemberController.java
@@ -2,34 +2,53 @@ package com.project.library.web.member;
 
 import com.project.library.domain.member.Member;
 import com.project.library.domain.member.MemberMapper;
+import com.project.library.web.SessionConst;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/member")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberMapper memberMapper;
 
     @GetMapping("/add")
-    public String addForm(@ModelAttribute("member") Member member) {
-        return "member/join";
+    public String joinForm(@ModelAttribute("member") Member member) {
+        return "members/joinForm";
     }
 
     @PostMapping("/add")
-    public String save(@Valid @ModelAttribute("member") Member member, BindingResult bindingResult) throws Exception {
+    public String join(@Valid @ModelAttribute("member") Member member, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "member/join";
+            return "members/joinForm";
+        }
+
+        Optional<Member> existLoginId = memberMapper.findByLoginId(member.getLoginId());
+        if (existLoginId.isPresent()) {
+            bindingResult.rejectValue("loginId", "duplicate", "이미 사용 중인 아이디입니다.");
+            return "members/joinForm";
         }
 
         memberMapper.insert(member);
         return "redirect:/";
     }
+
+    @GetMapping("/info")
+    public String memberInfo(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+        if (loginMember == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("member", loginMember);
+        return "members/memberInfo";
+    }
+
+
 }
