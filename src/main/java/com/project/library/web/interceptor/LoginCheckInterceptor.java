3828ea302f54987d6bb82a2e55e826bdd6902f74
songseongbin <ssbin0916@naver.com>
2024-03-23 17:05:50 +0900

    refactor: 간격 수정

--- src/main/java/com/project/library/web/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/project/library/web/interceptor/LoginCheckInterceptor.java
@@ -7,12 +7,9 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class LoginCheckInterceptor implements HandlerInterceptor {
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
         String requestURI = request.getRequestURI();
-
         HttpSession session = request.getSession();
 
         if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
