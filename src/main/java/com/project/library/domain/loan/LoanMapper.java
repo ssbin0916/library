f141c22afa3a9f7d907147fd03331a0e0a8e565c
songseongbin <ssbin0916@naver.com>
2024-03-25 02:07:38 +0900

    refactor: history 수정, delete 추가

--- src/main/java/com/project/library/domain/loan/LoanMapper.java
+++ src/main/java/com/project/library/domain/loan/LoanMapper.java
@@ -1,6 +1,6 @@
 package com.project.library.domain.loan;
 
-import com.project.library.domain.book.Book;
+import com.project.library.web.loan.LoanBookDTO;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
@@ -14,16 +14,20 @@ public interface LoanMapper {
     @Update("UPDATE loan SET return_date = SYSDATE WHERE book_id = #{bookId} AND member_id = #{memberId}")
     void returnBook(@Param("bookId") Long bookId, @Param("memberId") Long memberId);
 
-    @Select("SELECT * FROM loan WHERE member_id = #{memberId}")
-    List<Loan> findLoanedBooksByMemberId(Long memberId);
-
-    @Select("SELECT COUNT(*) FROM loan WHERE book_id = #{bookId}")
-    int countActiveLoansForBook(Long bookId);
-
     @Update("UPDATE book SET is_rented = 'Y' WHERE id = #{bookId}")
     void isRented(Long bookId);
 
     @Update("UPDATE book SET is_rented = 'N' WHERE id = #{bookId}")
     void isReturned(Long bookId);
 
+    @Select("SELECT b.id AS bookId, b.title, b.author, b.category, l.member_id AS memberId, " +
+            "TO_CHAR(l.rent_date, 'YYYY-MM-DD HH24:MI:SS') AS rentDate, " +
+            "TO_CHAR(l.return_date, 'YYYY-MM-DD HH24:MI:SS') AS returnDate " +
+            "FROM loan l JOIN book b ON l.book_id = b.id " +
+            "WHERE l.member_id = #{memberId} " +
+            "ORDER BY l.rent_date DESC")
+    List<LoanBookDTO> histories(Long memberId);
+
+    @Delete("DELETE FROM loan WHERE id = #{id}")
+    void delete(Long id);
 }
