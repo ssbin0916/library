b8dc7a987bdbfa00a368c50949df5a57bb8bc40d
songseongbin <ssbin0916@naver.com>
2024-03-30 00:46:18 +0900

    refactor: 생성자 추가

--- src/main/java/com/project/library/domain/member/Member.java
+++ src/main/java/com/project/library/domain/member/Member.java
@@ -1,9 +1,15 @@
 package com.project.library.domain.member;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 
     private Long id;
