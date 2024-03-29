dd4aaa9fa774c51a22e9d3e4787a3a039b7ffe94
songseongbin <ssbin0916@naver.com>
2024-03-29 22:05:47 +0900

    feat: 불필요한 import 정리

--- src/main/java/com/project/library/web/HomeController.java
+++ src/main/java/com/project/library/web/HomeController.java
@@ -1,18 +1,13 @@
 package com.project.library.web;
 
 import com.project.library.domain.member.Member;
-import com.project.library.domain.member.MemberMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
-@RequiredArgsConstructor
 public class HomeController {
-
-    private final MemberMapper memberMapper;
 
     @GetMapping("/")
     public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
